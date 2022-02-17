398741e3e99ca1670f76799870b447e141cb446b
David Monsalve <dvmonsalve@gmail.com>
2022-02-17 16:11:31 -0600

    testing it out

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {
+	$("#testingthehover").hover(function() {
+		$(this).css("color", "blue");
+	}, 
+	function() {
+		$(this).hide();
+		//$(this).css("display", "none");
+	});
+
+
+$("button").click(function() {
+    alert("you are about to leave this page")
+
+})
+});
+
+console.log("Your index.js file is loaded correctly!");
+
